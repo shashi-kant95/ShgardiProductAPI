2cfa6029239c30754b15b2c4476231b0274a1da1
Shashi Kant <113066626+shashi-kant95@users.noreply.github.com>
2024-12-27 01:00:45 +0530

    added basic controller setup

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShgardiProductAPI.Repository.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShgardiProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController()
+        {
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return new List<Product>();
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            try{
+            return new Product();
+            }
+            catch{
+                throw;
+            }
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(int id, Product product)
+        {
+            return NoContent();
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            return NoContent();
+        }
+    }
+
+}
+
