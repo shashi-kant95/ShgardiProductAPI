934612a78c8325d924497d317b16cec37577315f
Shashi Kant <113066626+shashi-kant95@users.noreply.github.com>
2024-12-26 23:25:34 +0530

    added modal and DBContext

---
+++ Repository/Models/DBContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductApi.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
