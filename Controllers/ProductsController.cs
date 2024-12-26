3598d675130a03bcc6c12c550d7ab7630b2a2fa2
Shashi Kant <113066626+shashi-kant95@users.noreply.github.com>
2024-12-27 04:20:44 +0530

    added CRUD operation

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ProductApi.Repository.Models;
 using ShgardiProductAPI.Repository.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -16,48 +17,115 @@ namespace ShgardiProductAPI.Controllers
 
     public class ProductsController : ControllerBase
     {
-        public ProductsController()
+        private readonly DBContext _dbContext;
+        public ProductsController(DBContext dBContext)
         {
+            _dbContext = dBContext;
         }
 
         // GET: api/Products
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
-            return new List<Product>();
+            try
+            {
+                var products = await _dbContext.Products.ToListAsync();
+                return Ok(products);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Server Error: " + ex.Message);
+            }
         }
 
         // GET: api/Products/5
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetProduct")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            try{
-            return new Product();
+            try
+            {
+                if (id == null)
+                {
+                    return BadRequest("Id is required");
+                }
+                var prod = await _dbContext.Products.FindAsync(id);
+                if (prod == null)
+                {
+                    return NotFound("No Record Found");
+                }
+                return prod;
             }
-            catch{
-                throw;
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Server Error: " + ex.Message);
             }
         }
 
         // PUT: api/Products/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutProduct(int id, Product product)
+        public async Task<IActionResult> PutProduct(int id, [FromBody] Product product)
         {
-            return NoContent();
+            try
+            {
+                if (id != product.Id)
+                {
+                    return BadRequest("Invalid Input");
+                }
+                var existingProduct = await _dbContext.Products.FindAsync(id);
+                if (existingProduct == null)
+                {
+                    return NotFound($"Product with ID {id} not found.");
+                }
+                // Update properties
+                existingProduct.Name = product.Name;
+                existingProduct.Price = product.Price;
+                existingProduct.Description = product.Description;
+                await _dbContext.SaveChangesAsync();
+                return NoContent();
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Server Error: " + ex.Message);
+            }
         }
 
         // POST: api/Products
         [HttpPost]
-        public async Task<ActionResult<Product>> PostProduct(Product product)
+        public async Task<ActionResult<Product>> PostProduct([FromBody] Product product)
         {
-            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+            try
+            {
+                _dbContext.Products.Add(product);
+                await _dbContext.SaveChangesAsync();
+                return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Server Error: " + ex.Message);
+            }
         }
 
         // DELETE: api/Products/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteProduct(int id)
         {
-            return NoContent();
+            try
+            {
+                var prod = await _dbContext.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
+                if (prod == null)
+                {
+                    return NotFound("No Record Found");
+                }
+                _dbContext.Products.Remove(prod);
+                await _dbContext.SaveChangesAsync();
+                return NoContent();
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Server Error: " + ex.Message);
+            }
         }
     }
 
--- Repository/Models/Product.cs
+++ Repository/Models/Product.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace ShgardiProductAPI.Repository.Models
 {
     public class Product
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
